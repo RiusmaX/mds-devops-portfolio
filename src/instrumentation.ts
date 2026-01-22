09480abc6b9ca24a7ec02a381747d44072badc25
Marius <marius.sergent@gmail.com>
2026-01-22 16:29:27 +0100

    improve monitoring

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -3,6 +3,7 @@ import { registerOTel } from '@vercel/otel'
 export function register() {
   console.log('🚧 [Instrumentation] Registering OpenTelemetry...');
   console.log('🚧 [Instrumentation] Endpoint:', process.env.OTEL_EXPORTER_OTLP_ENDPOINT ? 'Defined' : 'Missing');
+  console.log('🚧 [Instrumentation] Headers:', process.env.OTEL_EXPORTER_OTLP_HEADERS ? 'Defined (' + process.env.OTEL_EXPORTER_OTLP_HEADERS.substring(0, 10) + '...)' : 'Missing');
   console.log('🚧 [Instrumentation] Service Name:', process.env.OTEL_SERVICE_NAME);
 
   registerOTel({
